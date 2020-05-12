419c37eeee63829bfb1118537db649d706501064
Rishabh2324 <rishabh2401jain@gmail.com>
2020-05-12 11:18:27 +0530

    added webpack

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Navbar from "../components/Reusable/navbar"
 import Footer from "../components/Reusable/footer"
-import "materialize-css/dist/css/materialize.min.css"
+
 import "./layout.css"
 import Particles from "react-particles-js"
 
